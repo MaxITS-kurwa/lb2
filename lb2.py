dc8ed93de85ac99e5a9b8730b17e3ebd13969712
MaxITS-kurwa <ncfu.maso@gmail.com>
2024-11-18 21:52:20 +0300

    Добавлен файл с написанной программой

---
+++ lb2.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    if n < 0:
+        return "Факториал для отрицательных чисел не определен."
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+number = int(input("Введите число для вычисления факториала: "))
+print(f"Факториал {number} равен: {factorial(number)}")
